a7a6662f77aba935fa4fd8c99dfb951754208ff7
Manin CHIEV <tedoracopilot@gmail.com>
2025-06-04 00:24:32 -0400

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,8 +63,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+      return res.status(200).json(books[isbn].reviews);
+    } else {
+      return res.status(404).json({message: "Book not found"});
+    }
+  });
 
 module.exports.general = public_users;
