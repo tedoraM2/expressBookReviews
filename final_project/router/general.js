3f0e6a0656fdd54a7732c8859040ab9dbe99df29
Manin CHIEV <tedoracopilot@gmail.com>
2025-06-04 00:56:13 -0400

    Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,4 +71,20 @@ public_users.get('/review/:isbn',function (req, res) {
     }
   });
 
+  //Task 6
+  public_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!users.find(user => user.username === username)) {
+        users.push({"username": username, "password": password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user. Username and password required."});
+  });
+
 module.exports.general = public_users;
